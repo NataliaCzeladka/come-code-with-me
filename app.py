712eea17423b879c845ccadb785075c7447a9779
NataliaCzeladka <natalia.czeladka@gmail.com>
2024-02-02 15:07:34 +0000

    Add Sign Up functonality

--- app.py
+++ app.py
@@ -25,9 +25,27 @@ def get_blog_posts():
     return render_template("blog_posts.html", blog_posts=blog_posts)
 
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    return render_template("register.html")
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("sign_up"))
+
+        sign_up = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(sign_up)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+    return render_template("sign_up.html")
 
 
 if __name__ == "__main__":
