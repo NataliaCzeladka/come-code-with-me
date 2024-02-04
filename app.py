3fe3bd15a01209f0c993dfb710931bc1e602ee40
NataliaCzeladka <natalia.czeladka@gmail.com>
2024-02-04 15:05:23 +0000

    Add session user welcome page

--- app.py
+++ app.py
@@ -50,6 +50,8 @@ def sign_up():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("welcome", username=session["user"]))
+
     return render_template("sign_up.html")
 
 
@@ -65,7 +67,10 @@ def sign_in():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}!".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "welcome", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -75,8 +80,15 @@ def sign_in():
             # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("sign_in"))
-
     return render_template("sign_in.html")
+
+
+@app.route("/welcome/<username>", methods=["GET", "POST"])
+def welcome(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("welcome.html", username=username)
 
 
 if __name__ == "__main__":
