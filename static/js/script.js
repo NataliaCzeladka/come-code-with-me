2b803e5a5838226a146938cd3b3c14d2cd1056d4
NataliaCzeladka <natalia.czeladka@gmail.com>
2024-02-10 20:52:16 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-  $(".sidenav").sidenav({edge: "right"});
-  
-  $(document).ready(function(){
-    $('.materialboxed').materialbox();
-  });
-
-  $(document).ready(function(){
-    $('.tooltipped').tooltip();
+  $(".sidenav").sidenav({ edge: "right" });
+  $('.materialboxed').materialbox();
+  $('.tooltipped').tooltip();
+  $(".datepicker").datepicker({
+    format: "dd mmmm, yyyy",
+    yearRange: 0,
+    i18n: {
+      done: "Select"
+    }
   });
 });
