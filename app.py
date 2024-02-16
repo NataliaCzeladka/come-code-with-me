971c520c08138acdef429c57a60fa9e5e0982b53
NataliaCzeladka <natalia.czeladka@gmail.com>
2024-02-16 01:33:16 +0000

    Add delete_comment functionality

--- app.py
+++ app.py
@@ -178,6 +178,14 @@ def add_comment(blog_post_id):
     return render_template("read_post.html")
 
 
+@app.route("/delete_comment/<comment_id>")
+def delete_comment(comment_id):
+    mongo.db.comments.delete_one({"_id": ObjectId(comment_id)})
+
+    flash("Comment Successfully Deleted")
+    return redirect(request.referrer)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
